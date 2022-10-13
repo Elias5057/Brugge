7fa468970417360f26b06d6fe1cc3819495e728d
Elias5057 <huysveld.elias@lln.athena-driehofsteden.be>
2022-10-13 10:56:53 +0200

    Initial commit

---
+++ JS/java.js
@@ -0,0 +1,18 @@
+var tf = 0
+//  tf = true false
+
+function navigatie()
+{
+    //als navigatiebalk verborgen is dan wordt het menu getoond
+    if (tf == 0) 
+    {
+        document.getElementById("smartnav").style.display = "block";
+        tf = 1;
+    }
+    //als menu reeds zichtbaar is dan wordt het menu verborgen
+    else if (tf = 1)
+    {
+        document.getElementById("smartnav").style.display = "none";
+        tf = 0;
+    }
+}
