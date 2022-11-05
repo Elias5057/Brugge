461c1e2359091fb7cf49a44d095ec52e26c2d813
Elias5057 <huysveld.elias@lln.athena-driehofsteden.be>
2022-11-05 16:37:55 +0100

    update navigatie
    navigatiebalk is een klein beetje aangepast

--- JS/java.js
+++ JS/java.js
@@ -7,12 +7,14 @@ function navigatie()
     if (tf == 0) 
     {
         document.getElementById("smartnav").style.display = "block";
+        document.getElementById("wrapper").style.position = "fixed"
         tf = 1;
     }
     //als menu reeds zichtbaar is dan wordt het menu verborgen
     else if (tf = 1)
     {
         document.getElementById("smartnav").style.display = "none";
+        document.getElementById("wrapper").style.position = "relative"
         tf = 0;
     }
 }
